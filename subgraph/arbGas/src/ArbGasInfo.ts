341b1e4d64df2bac54360716fbe30145814a7021
shotaro <shawtarohgn@gmail.com>
2023-12-20 09:17:52 -1000

    feat: hourly gas stats

--- subgraph/arbGas/src/ArbGasInfo.ts
+++ subgraph/arbGas/src/ArbGasInfo.ts
@@ -5,9 +5,10 @@ import {
   NodeInterface,
 } from "../generated/ArbGasInfo/NodeInterface"
 import {
-  blockInfo
+  blockInfo,
+  hourlyStat
 } from "../generated/schema"
-import { ethereum, Address} from '@graphprotocol/graph-ts'
+import { ethereum, Address, BigInt} from '@graphprotocol/graph-ts'
 
 
 export function handleBlock(block: ethereum.Block): void {
@@ -17,6 +18,11 @@ export function handleBlock(block: ethereum.Block): void {
   //let nodeInterface = NodeInterface.bind(nodeInferfacePrecompile.toAddress());
   //const batchNum = nodeInterface.try_findBatchContainingBlock(block.number);
   //const l1blockNum = nodeInterface.try_blockL1Num(block.number)
+  const gasBacklogTry = arbGas.try_getGasBacklog();
+  if (gasBacklogTry.reverted) {
+    // probably pre-nitro?
+    return;
+  }
   const gasBacklog = arbGas.getGasBacklog();
   const L1BaseFee = arbGas.getL1BaseFeeEstimate();
   const L1pricingsurplus = arbGas.getL1PricingSurplus();
@@ -33,4 +39,25 @@ export function handleBlock(block: ethereum.Block): void {
   if (!l1blockNum.reverted)
     info.l1BlockNumber = l1blockNum.value;*/
   info.save()
+
+  let stat = hourlyStat.load((block.timestamp.div(BigInt.fromI32(3600))).toHexString())
+  if (stat == null){
+    stat = new hourlyStat((block.timestamp.div(BigInt.fromI32(3600))).toHexString());
+    stat.timestamp = block.timestamp.div(BigInt.fromI32(3600)).times(BigInt.fromI32(3600));
+    stat.l1GasPriceSum = BigInt.fromI32(0);
+    stat.gasBlocklogSum = BigInt.fromI32(0);
+    stat.l1PricingSurplusSum = BigInt.fromI32(0);
+    stat.l2GasPriceSum = BigInt.fromI32(0);
+    stat.count = BigInt.fromI32(0);
+  }
+  stat.l1GasPriceSum = stat.l1GasPriceSum.plus(L1BaseFee)
+  stat.gasBlocklogSum = stat.gasBlocklogSum.plus(gasBacklog)
+  stat.l1PricingSurplusSum = stat.l1PricingSurplusSum.plus(L1pricingsurplus);
+  stat.l2GasPriceSum = stat.l2GasPriceSum.plus(block.baseFeePerGas as BigInt);
+  stat.count = stat.count.plus(BigInt.fromI32(1));
+  stat.l1GasPriceAvg = stat.l1GasPriceSum.div(stat.count);
+  stat.gasBlocklogAvg = stat.gasBlocklogSum.div(stat.count);
+  stat.l1PricingSurplusAvg = stat.l1PricingSurplusSum.div(stat.count);
+  stat.l2GasPriceAvg = stat.l2GasPriceSum.div(stat.count);
+  stat.save()
 }
